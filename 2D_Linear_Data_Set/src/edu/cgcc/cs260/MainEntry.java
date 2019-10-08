81c1bcf32b484c7c5308ee0432f8e3466077798b
Jeff McHale <46632270+jeffmchale@users.noreply.github.com>
2019-10-07 20:59:01 -0700

    Update MainEntry.java
    Added Header and Footer

--- 2D_Linear_Data_Set/src/edu/cgcc/cs260/MainEntry.java
+++ 2D_Linear_Data_Set/src/edu/cgcc/cs260/MainEntry.java
@@ -1,3 +1,16 @@
+//HEADER 
+//Program Name: jm_wk2_2dLinearDataSet
+//Author: Jeff McHale
+//Class: CS260 Fall 2019
+//Date: 10/07/2019
+//Description: The problem issued requested a specific set of asterisks (*) to be printed out to the console.
+//By creating two functions containing nested loops, certain parameters were implemented to ensure that the 
+//desired outcome was met. A third function took the modified array and prints the correct pattern to the console.
+
+
+
+
+
 package edu.cgcc.cs260;
 
 public class MainEntry {
@@ -10,3 +23,16 @@ public class MainEntry {
 	}
 
 }
+
+/* FOOTER
+
+*       	* * * * * * * *
+* *		  * * * * * *
+* * *		    * * * *
+* * * *		      * *
+* * * * * 	      * *
+* * * * * * 	      * *	
+* * * * * * *	      * *
+* * * * * * * *     * * * *
+
+*/
