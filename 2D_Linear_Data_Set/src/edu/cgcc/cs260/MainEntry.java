319ac6bd9dcb9c2a097d32e5dc59a056a0ef9f91
Jeff McHale <jeffmchale94@gmail.com>
2019-10-07 19:57:42 -0700

    Committing appropriate .java files for HW assignment 2.

---
+++ 2D_Linear_Data_Set/src/edu/cgcc/cs260/MainEntry.java
@@ -0,0 +1,12 @@
+package edu.cgcc.cs260;
+
+public class MainEntry {
+
+	public static void main(String[] args) {
+		//create and fill 2D array to print image
+		TwoDArray arr = new TwoDArray();
+		arr.printArray();
+
+	}
+
+}
---
+++ 2D_Linear_Data_Set/src/edu/cgcc/cs260/TwoDArray.java
@@ -0,0 +1,79 @@
+package edu.cgcc.cs260;
+
+/** This class creates a 2D array and fills it to make
+ *  a picture of a triangle and glass
+ * 
+ */
+public class TwoDArray {
+	
+	/**
+	 * 2D array to hold image of triangle and glass
+	 */
+	char[][] arr = new char[8][16];
+	
+	/**
+	 * Constructor to fill array
+	 */
+	public TwoDArray() {
+		fillTriangle();
+		fillGlass();
+	}
+	
+	/**
+	 * fills triangle into 2D array
+	 */
+	private void fillTriangle() {
+		for(int r = 0; r < arr.length; r++) {
+			for(int c = 0; c < 8; c++) {
+				if(r < c) {
+					arr[r][c] = ' ';
+				}
+				else {
+					arr[r][c] = '*';
+				}
+			}
+		}
+	}
+	
+	/**
+	 * fills glass into 2D array
+	 */
+	private void fillGlass() {
+		for(int r = 0; r < arr.length; r++) {
+			for(int c = 8; c < arr[r].length; c++) {
+				if(r == 0) {
+					arr[r][c] = '*';
+				}
+				else if(r == 1 && c > 8 && c < 15) {
+					arr[r][c] = '*';
+				}
+				else if(r == 2 && c > 9 && c < 14) {
+					arr[r][c] = '*';
+				}
+				else if( r > 2 && r < 8 && c > 10 && c < 13) {
+					arr[r][c] = '*';
+				}
+				else if(r == 7 && c > 9 && c < 14) {
+					arr[r][c] = '*';
+				}
+				else {
+					arr[r][c] = ' ';
+				}
+			}
+		}
+	}
+	
+	/**
+	 * prints 2D array to console
+	 */
+	public void printArray() {
+		for(int r = 0; r < arr.length; r++) {
+			for(int c = 0; c < arr[r].length; c++) {
+				System.out.print(arr[r][c]);
+			}
+			System.out.println();
+		}
+	}
+
+}
+	
